74ae90bffbf228f7505fe2410fbfb27280877362
HustonMmmavr <hustonmavr@gmail.com>
2017-09-16 17:14:02 +0300

    Started with API

--- interface/views.py
+++ interface/views.py
@@ -4,5 +4,22 @@ from django.template import Template
 from django.shortcuts import render, render_to_response
 from interface.form import LoginForm
 # Create your views here.
+# def signin(request):
+# 	 return render(request, "signin.html",{})
+
 def signin(request):
-	 return render(request, "signin.html",{})
+    redirect = request.GET.get('continue', '/')
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(redirect)
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            auth.login(request, form.cleaned_data['user'])
+            return HttpResponseRedirect(redirect)
+    else:
+        form = LoginForm()
+
+    return render(request, 'signin.html', {
+            'form': form,
+})
